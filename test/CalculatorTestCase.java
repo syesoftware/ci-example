a682b32b11daa0aa7a317fe66c886dc2cdec18ed
Ra Acosta <ra.acosta@gmail.com>
2014-11-11 17:41:40 -0600

    added some changes to file

--- test/CalculatorTestCase.java
+++ test/CalculatorTestCase.java
@@ -1,16 +1,30 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Before;
 
 
 public class CalculatorTestCase {
+	Calculator calc;
+
+	@Before
+	public void setup(){
+		calc = new Calculator();
+	}
 	
 	@Test
 	public void testZeroValues(){
-		Calculator calc = new Calculator();
+		
 		String val = "";	
 		
 		assertEquals(0, calc.Add(val)); 
+	}
+
+	@Test
+	public void testOneValueAdd(){
+		String val = "10,20";
+
+		assertEquals(30,calc.Add(val));
 	}
 
 	
